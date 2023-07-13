31b3a67c713c914fcf2c7a8c7875aa23b0ccbae3
gabriel-nadalin <131068505+gabriel-nadalin@users.noreply.github.com>
2023-07-12 22:27:52 -0300

    passo 3 funcionando

--- tcp.py
+++ tcp.py
@@ -1,6 +1,6 @@
 import asyncio
 from tcputils import *
-
+import random
 
 class Servidor:
     def __init__(self, rede, porta):
@@ -34,7 +34,7 @@ class Servidor:
         if (flags & FLAGS_SYN) == FLAGS_SYN:
             # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
             # TODO: talvez você precise passar mais coisas para o construtor de conexão
-            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao)
+            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao, seq_no)
             # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
             # fazer aqui mesmo ou dentro da classe Conexao.
             if self.callback:
@@ -48,10 +48,15 @@ class Servidor:
 
 
 class Conexao:
-    def __init__(self, servidor, id_conexao):
+    def __init__(self, servidor, id_conexao, seq_no):
         self.servidor = servidor
         self.id_conexao = id_conexao
         self.callback = None
+        self.seq_no = random.randint(0, 0xffff)
+        self.ack_no = seq_no + 1
+        src_addr, src_port, dst_addr, dst_port = id_conexao
+        self.servidor.rede.enviar(fix_checksum(make_header(dst_port, src_port, self.seq_no, self.ack_no, FLAGS_SYN + FLAGS_ACK), dst_addr, src_addr), src_addr)
+        self.seq_no += 1
         self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
         #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
 
@@ -63,7 +68,12 @@ class Conexao:
         # TODO: trate aqui o recebimento de segmentos provenientes da camada de rede.
         # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
         # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
-        print('recebido payload: %r' % payload)
+        src_addr, src_port, dst_addr, dst_port = self.id_conexao
+        if seq_no == self.ack_no and len(payload) > 0:
+            self.callback(self, payload)
+            self.ack_no += len(payload)
+            self.servidor.rede.enviar(fix_checksum(make_header(dst_port, src_port, self.seq_no, self.ack_no, FLAGS_ACK), dst_addr, src_addr), src_addr)
+            print('recebido payload: %r' % payload)
 
     # Os métodos abaixo fazem parte da API
 
@@ -81,6 +91,16 @@ class Conexao:
         # TODO: implemente aqui o envio de dados.
         # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o segmento
         # que você construir para a camada de rede.
+        src_addr, src_port, dst_addr, dst_port = self.id_conexao
+        while len(dados) > 0:
+            if len(dados) > MSS:
+                segmento = dados[0:MSS]
+                dados = dados[MSS:]
+            else:
+                segmento = dados
+                dados = []
+            self.servidor.rede.enviar(fix_checksum(make_header(dst_port, src_port, self.seq_no, self.ack_no, FLAGS_ACK) + segmento, dst_addr, src_addr), src_addr)
+            self.seq_no += len(segmento)
         pass
 
     def fechar(self):
