e37ceeafe38b52a7689b630500b52d2f22ca4be0
gabriel-nadalin <131068505+gabriel-nadalin@users.noreply.github.com>
2023-07-16 23:16:44 -0300

    passo 5 funcionando

--- tcp.py
+++ tcp.py
@@ -1,6 +1,7 @@
 import asyncio
 from tcputils import *
 import random
+from collections import OrderedDict
 
 class Servidor:
     def __init__(self, rede, porta):
@@ -22,7 +23,7 @@ class Servidor:
             flags, window_size, checksum, urg_ptr = read_header(segment)
 
         if dst_port != self.porta:
-            # Ignora segmentos que não são destinados à porta do nosso servidor
+            # Ignora unacked que não são destinados à porta do nosso servidor
             return
         if not self.rede.ignore_checksum and calc_checksum(segment, src_addr, dst_addr) != 0:
             print('descartando segmento com checksum incorreto')
@@ -55,17 +56,21 @@ class Conexao:
         self.seq_no = random.randint(0, 0xffff)
         self.ack_no = seq_no + 1
         self.fin = False
+        self.unacked = OrderedDict()
         src_addr, src_port, dst_addr, dst_port = id_conexao
         self.servidor.rede.enviar(fix_checksum(make_header(dst_port, src_port, self.seq_no, self.ack_no, FLAGS_SYN + FLAGS_ACK), dst_addr, src_addr), src_addr)
         self.seq_no += 1
-        #self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
+        self.base_seq = self.seq_no
+        self.timer = None
         #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
 
-    def reenviar(self, segmento):
-        self.servidor.rede.enviar(segmento)
+    def retransmitir(self):
+        src_addr, src_port, dst_addr, dst_port = self.id_conexao
+        key = list(self.unacked.keys())[0]
+        self.servidor.rede.enviar(fix_checksum(make_header(dst_port, src_port, key, self.ack_no, FLAGS_ACK) + self.unacked[key], dst_addr, src_addr), src_addr)
 
     def _rdt_rcv(self, seq_no, ack_no, flags, payload):
-        # TODO: trate aqui o recebimento de segmentos provenientes da camada de rede.
+        # TODO: trate aqui o recebimento de unacked provenientes da camada de rede.
         # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
         # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
         src_addr, src_port, dst_addr, dst_port = self.id_conexao
@@ -73,11 +78,19 @@ class Conexao:
             self.callback(self, b'')
             self.ack_no = seq_no + 1
             self.servidor.rede.enviar(fix_checksum(make_header(dst_port, src_port, self.seq_no, self.ack_no, FLAGS_ACK), dst_addr, src_addr), src_addr)
-        elif (flags & FLAGS_ACK) == FLAGS_ACK and ack_no == self.seq_no + 1:
-            if self.fin:
+        if (flags & FLAGS_ACK) == FLAGS_ACK:
+            if self.fin and ack_no == self.seq_no + 1:
                 del self.servidor.conexoes[self.id_conexao]
-            
-        elif seq_no == self.ack_no and len(payload) > 0:
+            elif ack_no > self.base_seq and len(self.unacked) > 0:
+                while len(self.unacked) > 0 and self.base_seq < ack_no:
+                    self.base_seq += len(self.unacked[list(self.unacked.keys())[0]])
+                    del self.unacked[list(self.unacked.keys())[0]]
+                if self.timer != None:
+                    if len(self.unacked) > 0:
+                        self.timer = asyncio.get_event_loop().call_later(1, self.retransmitir)
+                    else:
+                        self.timer.cancel()
+        if seq_no == self.ack_no and len(payload) > 0:
             self.callback(self, payload)
             self.ack_no += len(payload)
             self.servidor.rede.enviar(fix_checksum(make_header(dst_port, src_port, self.seq_no, self.ack_no, FLAGS_ACK), dst_addr, src_addr), src_addr)
@@ -100,17 +113,14 @@ class Conexao:
         # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o segmento
         # que você construir para a camada de rede.
         src_addr, src_port, dst_addr, dst_port = self.id_conexao
-        while len(dados) > 0:
-            if len(dados) > MSS:
-                segmento = dados[:MSS]
-                dados = dados[MSS:]
-            else:
-                segmento = dados
-                dados = []
-            segmento = fix_checksum(make_header(dst_port, src_port, self.seq_no, self.ack_no, FLAGS_ACK) + segmento, dst_addr, src_addr), src_addr
-            self.servidor.rede.enviar(segmento)
-            self.seq_no += len(segmento)
-            self.timer = asyncio.get_event_loop().call_later(1, self.reenviar(segmento))
+        self.base_seq = self.seq_no
+        for i in range(0, len(dados), MSS):
+            self.unacked[self.seq_no] = dados[i:i + MSS]
+            self.seq_no += len(dados[i:i + MSS])
+        for key in self.unacked.keys():
+            self.servidor.rede.enviar(fix_checksum(make_header(dst_port, src_port, key, self.ack_no, FLAGS_ACK) + self.unacked[key], dst_addr, src_addr), src_addr)
+        if self.timer == None:
+            self.timer = asyncio.get_event_loop().call_later(1, self.retransmitir)
         pass
 
     def fechar(self):
